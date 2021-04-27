45057d2c745c08eef18be65e0b9fb48e4a71610d
AK <balaajay19@gmail.com>
2021-04-27 15:46:39 +0530

    Created new blog page

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+  };
+
   return (
     <div className="create">
       <h2>Add a new blog!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label>Blog body:</label>
+        <textarea
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+          required
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="ajay">ajay</option>
+        </select>
+        <button>Add Blog!</button>
+      </form>
     </div>
   );
 };
